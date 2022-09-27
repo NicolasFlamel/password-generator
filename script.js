615dfd24334b6c3ff7c0f8ff0dbe991e257bcffa
Amauri <hamauri@aol.com>
2022-09-27 14:26:34 -0700

    added conditions code

--- script.js
+++ script.js
@@ -1,5 +1,30 @@
 // Assignment code here
 
+function generatePassword() {
+  var password;
+  var tempConditions = conditions();
+
+  return password;
+}
+
+function conditions() {
+  var conditions = {
+    charLength: 8,  // character length for pw
+    lwr: 'no',      // lowercase
+    upper: 'no',    // uppercase
+    number: 'no',   // number
+    special: 'no'   // speical
+  };
+
+  //prompts to ask user their conditions 
+  conditions.charLength = prompt("Please enter password length." + "\nMust be at least 8 characters and no more than 128 characters.");
+  conditions.lwr = prompt("Inlcude lowercase characters?" + "\nYes or No").toLowerCase();
+  conditions.upper = prompt("Inlcude uppercase characters?" + "\nYes or No").toLowerCase();
+  conditions.number = prompt("Inlcude numeric characters?" + "\nYes or No").toLowerCase();
+  conditions.special = prompt("Inlcude special characters?" + "\nYes or No").toLowerCase();
+
+  return conditions;
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
